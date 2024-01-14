b104e2b1633e06758ce4d03bad1733aa44923c27
Advait Trivedi <trivediadvait0@gmail.com>
2024-01-14 06:00:15 +0530

    setup typescript for backend

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config()
+import express from "express";
+
+const app: express.Express = express()
+
+app.use(express.json())
+
+app.get("/", (req: express.Request, res: express.Response) => {
+    console.log("request recieved");
+})
+
+app.listen(8080, "localhost", ()=>{
+    console.log("Server started listening on http://localhost:8080");
+})
